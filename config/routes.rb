b08147137b1f721881999709f98004dd571495b7
ogawa kiyoshi <daikiiiiiiin777@gmail.com>
2023-06-01 13:08:49 +0000

    [Add]

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,9 @@
 Rails.application.routes.draw do
-  get 'users/edit'
-  get 'users/index'
-  get 'users/show'
   devise_for :users
   root to: "homes#top"
   get 'homes/about' => "homes#about" ,as: "about"
   resources :books, only: [:new, :create, :edit, :index, :show, :destroy]
-  resources :users, only: [:index, :show, :edit, :destroy]
+  resources :users, only: [:index, :show, :edit]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 end
