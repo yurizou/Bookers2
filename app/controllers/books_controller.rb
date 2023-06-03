5bfce7baed846f9cc8d943f654472e2f07d24815
ogawa kiyoshi <daikiiiiiiin777@gmail.com>
2023-06-03 03:01:41 +0000

    [Add]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,19 +7,34 @@ class BooksController < ApplicationController
     @book = Book.new(params[:id])
     @book.user_id = current_user.id
     @book.save
+    flash[:notice] = "You have created book successfully."
     redirect_to book_path 
   end
 
   def index
+    @books = Book.all
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    flash[:notice] = "You have updated book successfully."
+    redirect_to book_path(book.id)
   end
 
   def show
     @book = Book.find(params[:id])
   end
   
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
   
   private
   
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,20 @@
 class UsersController < ApplicationController
   def edit
   end
+  
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    flash[:notice] = "You have updated user successfully."
+    redirect_to user_path(user.id)
+  end
 
   def index
   end
 
   def show
+    @user = User.find(params[:id])
   end
+  
+  
 end
