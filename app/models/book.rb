85c20f7622868c6a51c35b9c9421e4fd620fe4f8
ogawa kiyoshi <daikiiiiiiin777@gmail.com>
2023-06-07 13:49:44 +0000

    [Add]

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,14 @@
 class Book < ApplicationRecord
   belongs_to :user
+  
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      redirect_to book_path
+    else
+      render :new
+    end
+  
+  end
 end
