d88f3933b48e6a912fe90fddfb6e98af1960b46d
ogawa kiyoshi <daikiiiiiiin777@gmail.com>
2023-06-11 01:09:08 +0000

    [Add]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,16 +6,16 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    flash[:notice] = "You have created book successfully."
     if @book.save
       redirect_to book_path(@book.id)
+      flash[:notice] = "You have created book successfully."
     else
       @books = Book.all
       @user = current_user
       render :index
     end
   end
-
+  
   def index
     @books = Book.all
     @user = current_user
@@ -29,11 +29,13 @@ class BooksController < ApplicationController
 
   def update
     @book = Book.find(params[:id])
-    @book.update(book_params)
-    flash[:notice] = "You have updated book successfully."
+    if @book.update(book_params)
     redirect_to book_path(@book.id)
+     flash[:notice] = "You have updated book successfully."
+    else
+     render:edit
+    end
   end
-  
   
 
   def show
--- db/migrate/20230530081517_create_books.rb
+++ db/migrate/20230530081517_create_books.rb
@@ -3,7 +3,7 @@ class CreateBooks < ActiveRecord::Migration[6.1]
     create_table :books do |t|
       t.string :title
       t.string :body
-      t.string :user_id
+      t.integer :user_id
       t.timestamps
     end
   end
